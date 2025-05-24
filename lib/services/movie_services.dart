21f32571897cb5ea7693bf475b823d1fa93016e2
anas al-dharei <anas202255@gmail.com>
2025-05-24 12:44:21 +0300

    Fix apitoken and add new getmovie feature

--- lib/services/movie_services.dart
+++ lib/services/movie_services.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 class MovieService {
   
   Future<dynamic> getPopularMovies() async {
-    final apitoken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYzhkMjU4NTdkYTk2MDVjYTExODlkMWI3NGI1Y2JlYSIsIm5iZiI6MTc0ODA3MzcyNS40OTMsInN1YiI6IjY4MzE3Y2ZkODJmNmE4YTcxMTQxMzg3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.l8jSjjaIczKVTj-az3MOAd1ouu_Yn4G15jGwOYnGMnc';
+    final apitoken = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYzhkMjU4NTdkYTk2MDVjYTExODlkMWI3NGI1Y2JlYSIsIm5iZiI6MTc0ODA3MzcyNS40OTMsInN1YiI6IjY4MzE3Y2ZkODJmNmE4YTcxMTQxMzg3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.l8jSjjaIczKVTj-az3MOAd1ouu_Yn4G15jGwOYnGMnc';
     final headers = {
       'Authorization': 'Bearer $apitoken',
       'accept': 'application/json',
@@ -23,7 +23,7 @@ class MovieService {
   }
 
   Future<dynamic> getTopRatedMovies() async {
-    final apitoken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYzhkMjU4NTdkYTk2MDVjYTExODlkMWI3NGI1Y2JlYSIsIm5iZiI6MTc0ODA3MzcyNS40OTMsInN1YiI6IjY4MzE3Y2ZkODJmNmE4YTcxMTQxMzg3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.l8jSjjaIczKVTj-az3MOAd1ouu_Yn4G15jGwOYnGMnc';
+    final apitoken = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYzhkMjU4NTdkYTk2MDVjYTExODlkMWI3NGI1Y2JlYSIsIm5iZiI6MTc0ODA3MzcyNS40OTMsInN1YiI6IjY4MzE3Y2ZkODJmNmE4YTcxMTQxMzg3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.l8jSjjaIczKVTj-az3MOAd1ouu_Yn4G15jGwOYnGMnc';
     final headers = {
       'Authorization': 'Bearer $apitoken',
       'accept': 'application/json',
@@ -43,7 +43,7 @@ class MovieService {
   }
 
   Future<dynamic> getUpcomingMovies() async {
-    final apitoken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYzhkMjU4NTdkYTk2MDVjYTExODlkMWI3NGI1Y2JlYSIsIm5iZiI6MTc0ODA3MzcyNS40OTMsInN1YiI6IjY4MzE3Y2ZkODJmNmE4YTcxMTQxMzg3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.l8jSjjaIczKVTj-az3MOAd1ouu_Yn4G15jGwOYnGMnc';
+    final apitoken = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYzhkMjU4NTdkYTk2MDVjYTExODlkMWI3NGI1Y2JlYSIsIm5iZiI6MTc0ODA3MzcyNS40OTMsInN1YiI6IjY4MzE3Y2ZkODJmNmE4YTcxMTQxMzg3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.l8jSjjaIczKVTj-az3MOAd1ouu_Yn4G15jGwOYnGMnc';
     final headers = {
       'Authorization': 'Bearer $apitoken',
       'accept': 'application/json',
@@ -51,6 +51,26 @@ class MovieService {
 
     final response = await http.get(
       Uri.parse('https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1'),
+      headers: headers,
+    );
+
+    if (response.statusCode == 200) {
+      return response.body;
+    }
+    else {
+      throw Exception('Failed to load movies');
+    }
+  }
+
+  Future<dynamic> getSimilarMovies(int movieId) async {
+    final apitoken = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYzhkMjU4NTdkYTk2MDVjYTExODlkMWI3NGI1Y2JlYSIsIm5iZiI6MTc0ODA3MzcyNS40OTMsInN1YiI6IjY4MzE3Y2ZkODJmNmE4YTcxMTQxMzg3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.l8jSjjaIczKVTj-az3MOAd1ouu_Yn4G15jGwOYnGMnc';
+    final headers = {
+      'Authorization': 'Bearer $apitoken',
+      'accept': 'application/json',
+    };
+
+    final response = await http.get(
+      Uri.parse('https://api.themoviedb.org/3/movie/${movieId}/similar?language=en-US&page=1'),
       headers: headers,
     );
 
