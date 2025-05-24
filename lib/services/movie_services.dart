5aee97eb6389daffe5805dc91fcd55654a55d289
anas al-dharei <anas202255@gmail.com>
2025-05-24 12:30:08 +0300

    Craeting Movies Services API

---
+++ lib/services/movie_services.dart
@@ -0,0 +1,65 @@
+import 'package:http/http.dart' as http;
+
+class MovieService {
+  
+  Future<dynamic> getPopularMovies() async {
+    final apitoken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYzhkMjU4NTdkYTk2MDVjYTExODlkMWI3NGI1Y2JlYSIsIm5iZiI6MTc0ODA3MzcyNS40OTMsInN1YiI6IjY4MzE3Y2ZkODJmNmE4YTcxMTQxMzg3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.l8jSjjaIczKVTj-az3MOAd1ouu_Yn4G15jGwOYnGMnc';
+    final headers = {
+      'Authorization': 'Bearer $apitoken',
+      'accept': 'application/json',
+    };
+
+    final response = await http.get(
+      Uri.parse('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1'),
+      headers: headers,
+    );
+
+    if (response.statusCode == 200) {
+      return response.body;
+    }
+    else {
+      throw Exception('Failed to load movies');
+    }
+  }
+
+  Future<dynamic> getTopRatedMovies() async {
+    final apitoken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYzhkMjU4NTdkYTk2MDVjYTExODlkMWI3NGI1Y2JlYSIsIm5iZiI6MTc0ODA3MzcyNS40OTMsInN1YiI6IjY4MzE3Y2ZkODJmNmE4YTcxMTQxMzg3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.l8jSjjaIczKVTj-az3MOAd1ouu_Yn4G15jGwOYnGMnc';
+    final headers = {
+      'Authorization': 'Bearer $apitoken',
+      'accept': 'application/json',
+    };
+
+    final response = await http.get(
+      Uri.parse('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1'),
+      headers: headers,
+    );
+
+    if (response.statusCode == 200) {
+      return response.body;
+    }
+    else {
+      throw Exception('Failed to load movies');
+    }
+  }
+
+  Future<dynamic> getUpcomingMovies() async {
+    final apitoken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYzhkMjU4NTdkYTk2MDVjYTExODlkMWI3NGI1Y2JlYSIsIm5iZiI6MTc0ODA3MzcyNS40OTMsInN1YiI6IjY4MzE3Y2ZkODJmNmE4YTcxMTQxMzg3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.l8jSjjaIczKVTj-az3MOAd1ouu_Yn4G15jGwOYnGMnc';
+    final headers = {
+      'Authorization': 'Bearer $apitoken',
+      'accept': 'application/json',
+    };
+
+    final response = await http.get(
+      Uri.parse('https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1'),
+      headers: headers,
+    );
+
+    if (response.statusCode == 200) {
+      return response.body;
+    }
+    else {
+      throw Exception('Failed to load movies');
+    }
+  }
+
+}
